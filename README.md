232f6a9cbecd099db5a1c0a668aca21b99c3966b
zhouyu <zhouyu@techvision.com.cn>
2021-05-26 16:03:14 +0800

    add banner and application.yml

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Email：1803594347@qq.com
 
 GitHub：https://github.com/TheCodeYu/crm.git
 
-# Changlogs
+# Changelog
 
 ## 2021-05-26
 
+- Generator, tests and example.
+- add banner and application.yml
--- admin/src/main/java/com/mychip/crm/CrmApplication.java
+++ admin/src/main/java/com/mychip/crm/CrmApplication.java
@@ -4,12 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+/**
+ * 启动入口
+ * @author	zhouyu
+ */
 @SpringBootApplication
 public class CrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
+		System.out.println("(♥◠‿◠)ﾉﾞ  system启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
 	}
 
 }
