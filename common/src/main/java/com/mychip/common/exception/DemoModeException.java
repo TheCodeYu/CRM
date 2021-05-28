fd30d73409c6895c340253d65c328f969e75e3db
zhouyu <zhouyu@techvision.com.cn>
2021-05-28 10:09:35 +0800

    add GlobalExceptionConfig

---
+++ common/src/main/java/com/mychip/common/exception/DemoModeException.java
@@ -0,0 +1,16 @@
+package com.mychip.common.exception;
+
+/**
+ * 演示模式异常
+ *
+ * @author zhouyu
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
+
